f3209700d09e09d2894cda220561834e62350ad4
DENIS <MOHAX1983@gmail.com>
2018-09-29 22:13:25 +0300

    7zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	let zero = 0;
+ 	while (number > 0) {
+ 		number = Math.floor(number /5,);
+ 		zero += number;
+  }
+  return zero;
 }
